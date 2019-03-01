cd8b23ce0cc40a413ec2e0a1de8a328f7d8df5ba
luca-Serena <lucaserena35@gmail.com>
2019-03-01 16:40:04 +0100

    adding temp files

---
+++ tempDbscan/src/main/scala/Record.scala
@@ -0,0 +1,23 @@
+object State extends  Enumeration{
+  type State = Value
+  val Visited, Unvisited, Noise, ClusterMember = Value
+}
+
+import State._
+
+import scala.collection.mutable
+@SerialVersionUID(100L)
+class Record (val doc: String, var vector: Map[String, Double]) extends Serializable {
+  val tweet: String = doc
+  var weighsVector = vector.withDefaultValue(0.0)
+  var state: State = Unvisited
+  var neighbors: Set[Record] = Set()
+
+  def apply_idf (idf: Map[String, Double]): Unit = {
+    this.weighsVector.transform((key, n) => n * idf(key))
+  }
+
+  override def toString: String =  this.tweet + "\n" + this.weighsVector
+}
+
+
